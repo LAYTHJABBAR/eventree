7bc4d59692b59d34bee781a0ac531778f6cd951b
Jamaal <jamaal729@yahoo.com>
2019-12-23 23:51:32 +0000

    updated local from git pull

--- src/components/event/EventForm/EventForm.js
+++ src/components/event/EventForm/EventForm.js
@@ -1,45 +1,6 @@
 import React, { Component } from 'react';
 import { Segment, Form, Button } from 'semantic-ui-react';
 
-<<<<<<< HEAD
-class EventForm extends Component {
-  render() {
-    const {cancelFormOpen} = this.props;
-    return (
-      <Segment>
-        <Form>
-          <Form.Field>
-            <label>Event Title</label>
-            <input placeholder='First Name' />
-          </Form.Field>
-          <Form.Field>
-            <label>Event Date</label>
-            <input type='date' placeholder='Event Date' />
-          </Form.Field>
-          <Form.Field>
-            <label>City</label>
-            <input placeholder='City event is taking place' />
-          </Form.Field>
-          <Form.Field>
-            <label>Venue</label>
-            <input placeholder='Enter the Venue of the event' />
-          </Form.Field>
-          <Form.Field>
-            <label>Hosted By</label>
-            <input placeholder='Enter the name of person hosting' />
-          </Form.Field>
-          <Button positive type='submit'>
-            Submit
-          </Button>
-          <Button onClick={cancelFormOpen} type='button'>Cancel</Button>
-        </Form>
-      </Segment>
-    );
-  }
-}
-
-export default EventForm;
-=======
 export default class EventForm extends Component {
   render() {
     return (
@@ -74,4 +35,3 @@ export default class EventForm extends Component {
     )
   }
 }
->>>>>>> 00cacb801e6d5345965e74954aa5f9b6e9194a22
--- src/components/event/EventList/EventListItem.js
+++ src/components/event/EventList/EventListItem.js
@@ -1,54 +1,3 @@
-<<<<<<< HEAD
-import React, { Component } from 'react';
-import { Segment, Item, Icon, List, Button } from 'semantic-ui-react';
-import EventListAttendee from './EventListAttendee';
-
-class EventListItem extends Component {
-  render() {
-    const {event} = this.props;
-    return (
-      <Segment.Group>
-        <Segment>
-          <Item.Group>
-            <Item>
-              <Item.Image
-                size='tiny'
-                circular
-                src={event.hostPhotoURL}
-              />
-              <Item.Content>
-                <Item.Header as='a'>{event.title}</Item.Header>
-                <Item.Description>
-                  Hosted by <a>{event.hostedBy}</a>
-                </Item.Description>
-              </Item.Content>
-            </Item>
-          </Item.Group>
-        </Segment>
-        <Segment>
-          <span>
-            <Icon name='clock' /> {event.date} |
-            <Icon name='marker' /> {event.venue}
-          </span>
-        </Segment>
-        <Segment secondary>
-          <List horizontal>
-            {event.attendees.map(attendee => (
-              <EventListAttendee key={attendee.id} attendee={attendee} />
-            ))}
-          </List>
-        </Segment>
-        <Segment clearing>
-          <span>{event.description}</span>
-          <Button as='a' color='teal' floated='right' content='View' />
-        </Segment>
-      </Segment.Group>
-    );
-  }
-}
-
-export default EventListItem;
-=======
 import React, { Component } from 'react'
 import { Segment, Item, Icon, List, Button } from 'semantic-ui-react';
 import EventListAttendee from '../EventList/EventListAttendee';
@@ -96,4 +45,3 @@ export default class EventListItem extends Component {
     )
   }
 }
->>>>>>> 00cacb801e6d5345965e74954aa5f9b6e9194a22

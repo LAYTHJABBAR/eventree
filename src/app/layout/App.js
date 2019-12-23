7b789510f7ae895dba4e7e3765b0d94deb4eb173
Samir Parab <samirparab@yahoo.com>
2019-12-23 18:05:56 +0000

    initiate and set up react project

---
+++ src/app/layout/App.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import cuid from 'cuid';
+
+
+
+class App extends Component {
+  
+  render () {
+    return (
+      <div>
+        <h1>Eventree</h1>
+
+      </div>
+    );
+
+  }
+}
+
+
+
+
+
+
+export default App;

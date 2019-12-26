2d96b0c2e984648ce4a4baa459586b462bf31bfa
LAYTHJABBAR <53024934+LAYTHJABBAR@users.noreply.github.com>
2019-12-26 16:27:04 -0700

    login and register form established

--- src/app/reducers/rootReducer.js
+++ src/app/reducers/rootReducer.js
@@ -1,10 +1,12 @@
 import { combineReducers } from "redux";
 import {reducer as FormReducer} from 'redux-form';
 import eventReducer from "../../components/event/eventReducer";
+import modalReducer from "../../components/Modals/modalReducer"
   
 export const rootReducer = combineReducers({
   // test: testReducer,
   form: FormReducer,
-  events: eventReducer
+  events: eventReducer,
+  modals: modalReducer
 })
 
--- src/components/nav/NavBar/NavBar.js
+++ src/components/nav/NavBar/NavBar.js
@@ -1,40 +1,69 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux'
 import { Menu, Container, Button } from 'semantic-ui-react';
 import { NavLink, Link, withRouter } from 'react-router-dom';
 import SignedOutMenu from '../Menus/SignedOutMenu';
 import SignedInMenu from '../Menus/SignedInMenu';
+import { openModal } from '../../Modals/modalActions';
 
+
+
+const actions = {
+  openModal,
+  
+}
 
 class NavBar extends Component {
   state = {
-    authenticated: true
+    authenticated: false
+  }
+  handleSignIn = () => {
+    this.props.openModal('LoginModal');
+  };
+
+  handleRegister = () => {
+    this.props.openModal('RegisterModal')
   }
 
-  handleSignIn = () => this.setState({ authenticated: true});
   handleSignOut = () => {
-    this.setState({ authenticated: false});
+  this.setState({
+    authenticated: false
+  })
     this.props.history.push('/');
-  }
+  };
 
   render() {
     const {authenticated} = this.state;
     return (
-            <Menu inverted fixed="top">
-              <Container>
-                <Menu.Item as={NavLink} exact to='/' header>
-                  <img src="/assets/eventree.png" alt="logo" />
-                  Eventree
-                </Menu.Item>
-                <Menu.Item as={NavLink} exact to='/events' name="Events List" />
-                <Menu.Item as={NavLink} to='/people' name="People" />
-                <Menu.Item>
-                  <Button as={Link} to="/createEvent" floated="right" positive inverted content="Create Event" />
-                </Menu.Item>
-                {authenticated ? < SignedInMenu signOut={this.handleSignOut}/> : <SignedOutMenu signIn={this.handleSignIn}/>}                
-              </Container>
-            </Menu>
-    )
+      <Menu inverted fixed='top'>
+        <Container>
+          <Menu.Item as={NavLink} exact to='/' header>
+            <img src='/assets/eventree.png' alt='logo' />
+            Re-vents
+          </Menu.Item>
+          <Menu.Item as={NavLink} exact to='/events' name='Events' />
+          <Menu.Item as={NavLink} to='/people' name='People' />
+      
+          {authenticated && 
+          <Menu.Item>
+            <Button
+              as={Link}
+              to='/createEvent'
+              floated='right'
+              positive
+              inverted
+              content='Create Event'
+            />
+          </Menu.Item>}
+          {authenticated ? (
+            <SignedInMenu signOut={this.handleSignOut} />
+          ) : (
+            <SignedOutMenu signIn={this.handleSignIn} register={this.handleRegister} />
+          )}
+        </Container>
+      </Menu>
+    );
   }
 }
 
-export default withRouter(NavBar);
+export default withRouter(connect(null, actions)(NavBar));

2b1ef6cfa2857b3fd8fe928b2a2a01fbef837688
Samir Parab <samirparab@yahoo.com>
2020-01-02 22:41:52 +0000

    event update instance

--- src/components/event/eventActions.js
+++ src/components/event/eventActions.js
@@ -34,16 +34,42 @@ export const createEvent = event => {
 };
 
 export const updateEvent = event => {
-  return async (dispatch, getState, { getFirestore }) => {
-    const firestore = getFirestore();
+  return async (dispatch, getState) => {
+    const firestore = firebase.firestore();
     try {
-      await firestore.update(`events/${event.id}`, event);
-      toastr.success("done", "event updated");
+      dispatch(asyncActionStart())
+      let eventDocRef = firestore.collection('events').doc(event.id);
+      let dateEqual = getState().firestore.ordered.events[0].date.isEqual(event.date);
+      if (!dateEqual) {
+        let batch = firestore.batch();
+        batch.update(eventDocRef, event);
+
+        let eventAttendeeRef = firestore.collection('event_attendee');
+        let eventAttendeeQuery = await eventAttendeeRef.where('eventId', '==', event.id);
+        let eventAttendeeQuerySnap = await eventAttendeeQuery.get();
+
+        for (let i=0; i<eventAttendeeQuerySnap.docs.length; i++) {
+          let eventAttendeeDocRef = firestore 
+            .collection('event_attendee')
+            .doc(eventAttendeeQuerySnap.docs[i].id);
+
+          batch.update(eventAttendeeDocRef, {
+            eventDate: event.date
+          })
+        } 
+        await batch.commit();
+      } else {
+        await eventDocRef.update(event);
+      }
+      dispatch(asyncActionFinish())
+      toastr.success('Success!', 'Event has been updated');
     } catch (error) {
-      toastr.error("fail", "try again");
+      dispatch(asyncActionError())
+      toastr.error('Oops', 'Something went wrong');
     }
   };
 };
+
 
 export const cancelToggle = (cancelled, eventId) =>
 async (dispatch, getState, {getFirestore}) => {

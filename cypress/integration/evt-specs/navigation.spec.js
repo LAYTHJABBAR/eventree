4bf55d1e5f3c1290abbcc3b64097a00e7b82c431
Jamaal <jamaal729@yahoo.com>
2020-01-04 22:16:48 -0700

    Fix cypress directory structure

--- cypress/integration/evt-specs/navigation.spec.js
+++ cypress/integration/evt-specs/navigation.spec.js
@@ -3,22 +3,19 @@ describe("Navigation", () => {
     cy.visit("/");
   });
 
-
   it("should navigate to Events", () => {
-    // const { container, debug } = <Application />;
     cy.visit("/");
   });
 
   it("should select an event", () => {
-    // const { container, debug } = <Application />;
-
-    cy.wait(3000);
+    cy.wait(1000);
     cy.visit("/events");
     cy.get(".ui.grid");
-    // cy.get(".ui clearing segment");
+
     cy.get(".ui.purple.button");
     cy.get("a.ui.purple.floated.right.button:first").click();
     cy.get("div.ui.segments");
     cy.get("div.ui.basic.top.attached.segment").should("be.visible");
   });
 });
+

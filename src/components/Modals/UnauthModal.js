9ed8ee4084fee58a08e36a22b96aa91cf0c632ac
LAYTHJABBAR <53024934+LAYTHJABBAR@users.noreply.github.com>
2020-01-08 16:05:01 -0700

    styling for login form

--- src/components/Modals/UnauthModal.js
+++ src/components/Modals/UnauthModal.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Modal, Button, Divider } from "semantic-ui-react";
+import { Modal, Button } from "semantic-ui-react";
 import { connect } from "react-redux";
 import { withRouter } from "react-router-dom";
 
@@ -21,27 +21,38 @@ class UnauthModal extends Component {
     const { openModal } = this.props;
     return (
       <Modal size="mini" open={true} onClose={this.handleCloseModal}>
-        <Modal.Header>You need to be signed in to do that!</Modal.Header>
+        <Modal.Header style={{color: 'purple'}}>Please Sign In First!!</Modal.Header>
         <Modal.Content>
           <Modal.Description>
             <p>Please either login or register to see this page</p>
-            <Button.Group widths={4}>
+
+            <div class="ui one column stackable center aligned page grid">
+   <div class="column twelve wide">
+     
+          <Button.Group class="ui center aligned" >
               <Button
-                fluid
-                color="teal"
+            
+                floated
+                color="purple"
                 onClick={() => openModal("LoginModal")}
+               
               >
                 Login
               </Button>
               <Button.Or />
-              <Button fluid positive onClick={() => openModal("RegisterModal")}>
+              <Button 
+            floated
+               color='red' onClick={() => openModal("RegisterModal")}>
                 Register
               </Button>
-            </Button.Group>
-            <Divider />
+              </Button.Group>
+   </div>
+</div>
+           
+         
             <div style={{ textAlign: "center" }}>
               <p>Or click cancel to continue as a guest</p>
-              <Button onClick={this.handleCloseModal}>Cancel</Button>
+              <Button circular onClick={this.handleCloseModal}>Cancel</Button>
             </div>
           </Modal.Description>
         </Modal.Content>
